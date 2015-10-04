392923aacc84e20e33c3e03a72fd6d4b6cfc624b
sinshev <sinshev@cogniance.com>
2015-10-04 14:31:48 +0300

    another version of 9. done 10

--- Python/10_sum_of_primes.py
+++ Python/10_sum_of_primes.py
@@ -1,20 +1,31 @@
 # Problem 10
 
 def is_prime(k):
-	for i in xrange(2,k):
+	for i in xrange(7,k,2):
 		if k%i==0:
 			return False
 	return True
 
 def sum_of_primes(n):
-	s = 2+3
-	for i in xrange(4,n):
-		if is_prime(i):
-			s = s+i
-			print s
+	s = 2+3+5 # first 3 primes
+	lst = [i for i in xrange(7,n,2) if i%3!=0 and i%5!=0]
+	for x in lst:
+		if is_prime(x):
+			s = s+x
 	print s
 			
-# sum_of_primes(2000000)
-# lst = [2*i-1 for i in xrange(2,1000000)]
-# lst = (2*i-1 for i in xrange(2,1000000))
-lst = [i for i in xrange(2,1000000,2)
+# sum_of_primes(2000000) # takes approx. 10 minutes
+
+def sum_of_primes_2(n):
+    sieve = [True] * n
+    s = 0
+    current_prime = 2
+    while current_prime < n:
+        if sieve[current_prime]:
+            s = s + current_prime
+            for i in xrange(current_prime**2, n, current_prime):
+                sieve[i] = False
+        current_prime += 1
+    return s
+
+print sum_of_primes_2(2000000) # takes 4 seconds
--- Python/9_pythagorean_triplet.py
+++ Python/9_pythagorean_triplet.py
@@ -11,4 +11,22 @@ def triplet():
 				if a<b<c and a*a+b*b==c*c and a+b+c==1000:
 					return a*b*c
 
-print triplet()
+# print triplet() # takes 45 seconds
+
+from itertools import permutations, product
+
+def triplet_permutations():
+	a = permutations(range(1, 500), 3)
+	for i in a:
+		if sum(i) == 1000 and i[0]<i[1]<i[2] and i[0]*i[0] + i[1]*i[1] == i[2]*i[2]:
+			return i[0]*i[1]*i[2]
+
+# print triplet_permutations() # takes 25 seconds
+
+def triplet_product(): 
+	a = product(range(1, 500), range(1, 500), range(1, 500))
+	for i in a:
+		if sum(i) == 1000 and i[0]<i[1]<i[2] and i[0]*i[0] + i[1]*i[1] == i[2]*i[2]:
+			return i[0]*i[1]*i[2]
+
+# print triplet_product() # takes 25 seconds
