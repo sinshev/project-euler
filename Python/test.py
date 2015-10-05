7bd1801b2eda19293bb46d914734a44e8eca9590
sinshev <sinshev@cogniance.com>
2015-10-06 01:55:29 +0300

    12 started

--- Python/test.py
+++ Python/test.py
@@ -1,19 +1,26 @@
 
 def get_divisors(n):
-	divisors_count = 0
-	for i in xrange(1,n/2):
-		if n%i == 0:
-			divisors_count += 1
-	return divisors_count
+	sieve = [1] * (n/2)
+	if n%2 != 0:
+		sieve[::2] = [False] * (n/4)
+	for i in xrange(1, len(sieve)):
+		if sieve[i] and n%i != 0:
+			sieve[i] = 0
+	return sum(sieve)
+
+	# divisors_count = 0
+	# for i in xrange(1,n/2):
+	# 	if n%i == 0:
+	# 		divisors_count += 1
+	# return divisors_count
 
 def triangle_number():	
 	number = 0
 	a = 0
-	while a <= 500:
-		for i in xrange(2, 999999999999, 2):
-			number = number + i
-			# print number
-			a = get_divisors(number) 
-	return number
+	for i in xrange(2, 999999999999, 2):
+		number = number + i
+		a = get_divisors(number)
+		if a >= 500:
+			return number
 
 triangle_number()
